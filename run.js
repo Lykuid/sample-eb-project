2efc40504a5699e2441ca1f613827c4053836821
David Maust <david@dmaust.net>
2017-03-12 17:57:04 -0700

    Initial Commit: Sample EB Project

---
+++ run.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var http = require('http');
+var app = express();
+var debug = require('debug')('sample-eb-app:server');
+var server = http.createServer(app);
+
+var port = 8080;
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  res.send('hello world')
+});
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
